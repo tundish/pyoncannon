3afba5462082fc8575e0f9bf81c84e23ebf2cb4f
Tundish <tundish@thuswise.org>
2015-04-20 18:50:34 +0100

    Added first test module.

---
+++ test_yardstick.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# encoding: UTF-8
+
+# This file is part of turberfield.
+#
+# Turberfield is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Turberfield is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with turberfield.  If not, see <http://www.gnu.org/licenses/>.
+
+from collections import OrderedDict
+from io import StringIO
+import textwrap
+import unittest
+
+class VimrcTests(unittest.TestCase):
+
+    data = textwrap.dedent("""
+        set shiftwidth=4
+        set tabstop=4
+        set textwidth=79
+        set expandtab
+        set backspace=2
+        set number
+        set ruler
+    """)
+
+    def test_top_content(self):
+        self.fail(VimrcTests.data)
